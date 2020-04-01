1e1c4a413d92f3ea6f76fcc898ad708e96fb3cf2
Ahmed Rachid <58266339+ahmedrachid@users.noreply.github.com>
2020-04-01 13:50:04 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,49 +1,17 @@
 import http.server
 import socketserver
+from http import HTTPStatus
+from tweets import Tweets
+import json
+tweet = Tweets()
 
-import pandas as pd 
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        if self.path == '/':
+            self.wfile.write(json.dumps(tweet.get_number_tweets_countries()))
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-
-
-class CostumHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-        def do_GET(self):
-                self.send_response(200)
-
-                self.send_header("Content-type", "text/html")
-                
-                self.end_headers()
-                query_components = parse_qs(urlparse(self.path))
-
-                # get the value of self.path 
-                if self.path == '/basicfilter':
-                        data = pd.read_csv("tweets.csv")
-                        print("hello")
-                        html = pd.DataFrame.to_html(data)
-                        
-
-                        self.wfile.write(bytes(html, "utf8"))
-                        return 
-                #return http.server.SimpleHTTPRequestHandler.do_GET(self)
-                # get the value of parameters .... 
-
-                #do the right thing depending on the parameters value ... 
-
-
-
-
-
-
-
-
-
-
-PORT = 8000
-handler = CostumHttpRequestHandler
-
-
-
-with socketserver.TCPServer(("", PORT), handler) as httpd: 
-        print("Server started at localhost:" +str(PORT))
-        httpd.serve_forever()
+httpd = socketserver.TCPServer(('', 8000), Handler)
+httpd.serve_forever()
