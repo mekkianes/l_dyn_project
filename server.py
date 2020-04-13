ca18317f973463380ec66c306e335cfcf2186502
Ahmed Rachid <58266339+ahmedrachid@users.noreply.github.com>
2020-04-13 16:10:50 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,17 +1,49 @@
 import http.server
 import socketserver
 from http import HTTPStatus
+from urllib.parse import urlparse, parse_qs
+from urllib import parse
 from tweets import Tweets
 import json
+
 tweet = Tweets()
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        if self.path == '/':
-            self.wfile.write(json.dumps(tweet.get_number_tweets_countries()))
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+        path = parse.urlparse(self.path).path
+        params = parse_qs(urlparse(self.path).query)
+
+        services_list = ['/', '/country_tweets']
+        if path in services_list:
+            if path == '/':
+                f = open('index.html','rb')
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+
+            if path == '/country_tweets':
+                if 'country' in params:
+                    country = params['country'][0]
+                json_result = tweet.get_number_tweets_country(country)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/json')
+                self.end_headers()
+                self.wfile.write(bytes(json.dumps(json_result), "utf8"))
+        
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            html = f"<html><head></head><body><h1>404 ERROR !!!!</h1></body></html>"
+            self.wfile.write(bytes(html, "utf8"))
+        return
 
 httpd = socketserver.TCPServer(('', 8000), Handler)
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print('Shut the server down')
+    httpd.socket.close()
--- tweets.py
+++ tweets.py
@@ -16,8 +16,77 @@ class Tweets:
         for index, row in tmp.iteritems():
             result[index] = row
         return result
-    def get_number_tweets_country(self, country):
-        tmp = self.df[self.df['place_country']==country]
-        total_tweets = len(tmp)
-        return tmp.to_json(orient='records')
 
+    def countWords(self, a_list):
+        words = {}
+        for i in range(len(a_list)):
+            item = a_list[i]
+            count = a_list.count(item)
+            words[item] = count
+        sorted_list = sorted(words.items(), key = lambda item: item[1], reverse=True)
+        return sorted_list[:10]
+    def hashtags_count(self, df):
+        list_hashtags = []
+        list_hashtags.extend(df['hashtag_0'].dropna().values.tolist())
+        list_hashtags.extend(df['hashtag_1'].dropna().values.tolist())
+        list_hashtags.extend(df['hashtag_2'].dropna().values.tolist())
+        
+        result = self.countWords(list_hashtags)
+        return result
+    def hashtags_country(self, country):
+        if country != '':
+            hashtag_count = self.hashtags_count(self.df[self.df['place_country']==country])
+        else:
+            hashtag_count = self.hashtags_count(self.df)
+        hashtags_items = [i[0] for i in hashtag_count]
+        hashtags_values = [i[1] for i in hashtag_count]
+        return hashtags_items, hashtags_values
+
+    def places_country_tweets(self, df):
+        
+        places_df = df.groupby('place_name').count().sort_values(by='id', ascending = False)
+        places_country = places_df.index.tolist()[:10]
+        places_count = places_df['id'].values.tolist()[:10]
+        
+        return places_country, places_count
+
+    def map_tweets(self, df):
+        longtitude = df['longitude'].values.tolist()
+        latitude = df['latitude'].values.tolist()
+        return longtitude, latitude
+
+    def language_tweets(self, df):
+        lang_df = df.groupby('lang').count().sort_values(by='id', ascending = False)
+        lang_country = lang_df.index.tolist()[:10]
+        lang_count = lang_df['id'].values.tolist()[:10]
+        
+        return lang_country, lang_count
+
+    def get_number_tweets_country(self, country):
+        if country != '':
+            tmp = self.df[self.df['place_country']==country]
+        else:
+            tmp = self.df
+        total_tweets = len(tmp)
+        
+        dic = dict()
+        dic['length'] = total_tweets
+        
+        hashtags_items, hashtags_values = self.hashtags_country(country)
+        dic['hashtags'] = hashtags_items
+        dic['hashtags_count'] = hashtags_values
+        
+        places_country, places_count = self.places_country_tweets(tmp)
+        dic['places_country'] = places_country
+        dic['places_count'] = places_count
+
+        lang_country, lang_count = self.language_tweets(tmp)
+        dic['lang_country'] = lang_country
+        dic['lang_count'] = lang_count
+
+        longitude, latitude  = self.map_tweets(tmp)
+        dic['longitude'] = longitude
+        dic['latitude'] = latitude
+
+        return dic
+        #return tmp.to_dict(orient="records")
