dee898e75c2e0f69cd3e98ebc0788d3e103416af
Ahmed Rachid <58266339+ahmedrachid@users.noreply.github.com>
2020-04-25 17:58:55 +0200

    Add files via upload

--- client.js
+++ client.js
@@ -7,6 +7,23 @@ let submit_button = document.getElementById("submitButton");
 
 console.log('Client 1');
 
+const timeoutPromise = function(ms, promise){
+
+    // Creer une promesse qui est rejeter au bout de ms en millisecondes
+    let timeout = new Promise((resolve, reject) => {
+      let id = setTimeout(() => {
+        clearTimeout(id);
+        reject('Timed out in '+ ms + 'ms.')
+      }, ms)
+    })
+  
+    // race retourne la promesse qui se manifeste en premier CAD qui retourne soit un resolve ou un reject
+    return Promise.race([
+      promise,
+      timeout
+    ])
+  }
+  
 Client.ajax = function (method, url) {
     return new Promise ((resolve, reject) => {
         let xhr = new XMLHttpRequest();
@@ -41,18 +58,41 @@ Client.query = async function (params) {
         + paramString;
     console.log("url est ::",url)
     try{
-        res = await Client.ajax("GET", url)
-        return res;
+        res = await timeoutPromise(5000,Client.ajax("GET", url));
+        return JSON.parse(res);
     }catch(str){
         alert("ERROR")
         res =str
+        return res;
     }
-    return res;
 };
 submit_button.addEventListener("click",function(){
     console.log("helllooooo");
     let jsonexample_param={"country":country_input.value};
     console.log(Client.query(jsonexample_param));
-    
+    let results = Client.query(jsonexample_param).then(function(result) {
+        console.log('!!');
+        console.log(result);
+        var myCanvas = document.getElementById("myCanvas");
 
+        var hashtag_keys = result['hashtags'];
+        var hashtag_values = result['hashtags_count'];
+
+        var hashtags = {};
+        hashtag_keys.forEach((key, i) => hashtags[key] = hashtag_values[i]);
+        console.log(hashtags);
+
+        var myBarchart = new Barchart(
+            {
+                canvas:myCanvas,
+                seriesName:"Vinyl records",
+                padding:20,
+                gridScale:5,
+                gridColor:"#eeeeee",
+                data:hashtags,
+                colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
+            }
+        );
+        myBarchart.draw();
+    });
 });
--- graph.js
+++ graph.js
@@ -122,4 +122,4 @@ var myBarchart = new Barchart(
         colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
     }
 );
-myBarchart.draw();
+//myBarchart.draw();
